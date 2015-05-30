19e68b5cb809f75c441974253a8a9362beee4b97
jackzampolin <jack.zampolin@gmail.com>
2015-05-29 18:28:25 -0700

    passing to edwin

--- app/models/http.rb
+++ app/models/http.rb
@@ -62,7 +62,7 @@ class DB_SEEDER
 
     def fk_key
       keys = [ ENV['FORECAST_KEY1'], ENV['FORECAST_KEY2'], ENV['FORECAST_KEY3'], ENV['FORECAST_KEY4'] ]
-      if CALLS % 999 == 0
+      if @@calls % 999 == 0
         @@fk_key_num += 1
       end
       keys[@@fk_key_num]
@@ -70,7 +70,7 @@ class DB_SEEDER
 
     def wu_key
       keys = [ ENV['WUAPI_KEY1'], ENV['WUAPI_KEY2'], ENV['WUAPI_KEY3'], ENV['WUAPI_KEY4'] ]
-      if CALLS % 499 == 0
+      if @@calls % 499 == 0
         @@wu_key_num += 1
       end
       keys[@@wu_key_num]
@@ -97,31 +97,45 @@ class DB_SEEDER
     end
 
     def make_forecast(wu,fk,city)
-      apparent_temps = fk['hourly']['data'].map do |for_hash|
+      apparent_temps = fk.body['hourly']['data'].map do |for_hash|
         for_hash['apparentTemperature'].to_i
       end
       avg_apparent_temp = apparent_temps.reduce(:+)/apparent_temps.length
       WeatherDay.create(
         city_id: city.id,
-        date: fk['daily']['data'][0]['time'],
-        summary: fk['daily']['data'][0]['summary'],
-        sunrise: fk['daily']['data'][0]['sunriseTime'],
-        sunset: fk['daily']['data'][0]['sunsetTime'],
-        cloud_cover: fk['daily']['data'][0]['cloudCover'],
-        rain: wu['history']['dailysummary'][0]['rain'],
-        precipitation: wu['history']['dailysummary'][0]['precipi'],
-        snow: wu['history']['dailysummary'][0]['snow'],
-        snowfall: wu['history']['dailysummary'][0]['snowfalli'],
-        avg_temp: wu['history']['dailysummary'][0]['meantempi'],
-        high_temp: wu['history']['dailysummary'][0]['maxtempi'],
-        low_temp: wu['history']['dailysummary'][0]['mintempi'],
-        avg_dewpt: wu['history']['dailysummary'][0]['meandewpti'],
-        avg_wind_spd: wu['history']['dailysummary'][0]['meanwindspdi'],
-        avg_wind_dir: wu['history']['dailysummary'][0]['meanwdird'],
-        humidity: wu['history']['dailysummary'][0]['humidity'],
-        high_apparent_temp: fk['daily']['data'][0]['apparentTemperatureMax'],
-        low_apparent_temp: fk['daily']['data'][0]['apparentTemperatureMin'],
+        date: fk.body['daily']['data'][0]['time'],
+        #LAST WEATHERDAY OBJECTS WAS 4/29/15, MISSING 4/30. WAS IT CALLED?
+        #THEORY: EPOCH TIME IS ON GMT, SO WE NEED TO PUSH HOURS FORWARD
+        summary: fk.body['daily']['data'][0]['summary'],
+        sunrise: fk.body['daily']['data'][0]['sunriseTime'],
+        sunset: fk.body['daily']['data'][0]['sunsetTime'],
+        cloud_cover: fk.body['daily']['data'][0]['cloudCover'],
+        #CLOUD COVER NEEDS TO BE FLOAT(2)
+        rain: wu.body['history']['dailysummary'][0]['rain'],
+        #RAIN WAS TRUE, BUT WEB DATA SAYS FALSE. I CHECKED THE DAY AFTER (4/30), AND IT COMES UP AS TRUE, SO I THINK THE DATES ARE MISMATCHED
+        precipitation: wu.body['history']['dailysummary'][0]['precipi'],
+        #THIS IS FUCKING WEIRD. PRECIPI IS RETURNING A STRING CALLED 'T' IN THE WEB DATA. WE MAY NEED TO CALL THE PRECIP FROM FORECAST.IO. SHOULD ALSO BE A FLOAT, IN CASE LESS THAN .5 INCHES, ETC.
+        snow: wu.body['history']['dailysummary'][0]['snow'],
+        snowfall: wu.body['history']['dailysummary'][0]['snowfalli'],
+        #WILL LIKELY ALSO NEED TO BE A FLOAT(2)
+        avg_temp: wu.body['history']['dailysummary'][0]['meantempi'],
+        #SHOULD BE FINE AS AN INTEGER, BUT COULD BE FLOAT(2)
+        high_temp: wu.body['history']['dailysummary'][0]['maxtempi'],
+        #SHOULD BE FINE AS AN INTEGER, BUT COULD BE FLOAT(2)
+        low_temp: wu.body['history']['dailysummary'][0]['mintempi'],
+        #SHOULD BE FINE AS AN INTEGER, BUT COULD BE FLOAT(2)
+        avg_dewpt: wu.body['history']['dailysummary'][0]['meandewpti'],
+        avg_wind_spd: wu.body['history']['dailysummary'][0]['meanwindspdi'],
+        avg_wind_dir: wu.body['history']['dailysummary'][0]['meanwdird'],
+        humidity: wu.body['history']['dailysummary'][0]['humidity'],
+        high_apparent_temp: fk.body['daily']['data'][0]['apparentTemperatureMax'],
+        #SHOULD BE FINE AS AN INTEGER, BUT COULD BE FLOAT(2)
+        low_apparent_temp: fk.body['daily']['data'][0]['apparentTemperatureMin'],
+        #SHOULD BE FINE AS AN INTEGER, BUT COULD BE FLOAT(2)
         avg_apparent_temp: avg_apparent_temp.to_i
+        #RECEIVED 59. DID HOURLY TEMPS BY HAND, GOT 60.02
+        #WE SHOULD DEF THINK ABOUT GETTING AVG FROM JUST LIGHT HOURS, BECAUSE SAYING IT FEELS LIKE 59 WHEN THE HIGH IS 74 COULD BE CONFUSING FOR A USER. IF WE CUT OUT THE DARK HOURS WHEN IT'S COLDEST, THE DATA WON'T BE AS SKEWED.
+        #SINCE WE TALKED ABOUT IT, MAYBE DAYTIME_FEELS_LIKE, NIGHTTIME_FEELS_LIKE?
       )
     end
 
@@ -131,8 +145,8 @@ class DB_SEEDER
 
       return p message if date.day == 1 && date.month == 5 && date.year == 2015
 
-      wu_res = HTTP::WU.send_request(wu_args(city,date))
-      fk_res = HTTP::FK.send_request(fk_args(city,date))
+      wu_res = HTTP::WU.send_request(wu_args(date,city))
+      fk_res = HTTP::FK.send_request(fk_args(date,city))
 
       make_forecast(wu_res,fk_res,city)
 
