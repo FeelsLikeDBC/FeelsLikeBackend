cba9362a9f1e64318a18d038f867c3a7fb388dd9
Edwin Unger <edwin@ungermail.com>
2015-05-30 14:31:19 -0700

    heroku

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -3,6 +3,6 @@ class CityController < ApplicationController
   def index
     @city = City.first
     @weather1 = WeatherDay.first
-    render json: {city: @city, weather: @weather}
+    render json: {city: @city, weather: @weather1}
   end
 end
