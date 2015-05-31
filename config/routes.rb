9bc483be6a186a6bad2f3e229b8af3fb2d9def79
Edwin Unger <edwin@ungermail.com>
2015-05-31 16:55:45 -0700

    backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,23 +10,23 @@ Rails.application.routes.draw do
   match '/city/:id/feels_like_yearly' => "weather_day#feels_like_yearly", :via => [:get]
 
 # Actual Temp
-  # match '/city/:id/actual_temp' => "weather_day#actual_temp", :via => [:get]
-  # match '/city/:id/actual_temp_monthly' => "weather_day#actual_temp_monthly", :via => [:get]
-  # match '/city/:id/actual_temp_yearly' => "weather_day#actual_temp_yearly", :via => [:get]
+  match '/city/:id/actual_temp' => "weather_day#actual_temp", :via => [:get]
+  match '/city/:id/actual_temp_monthly' => "weather_day#actual_temp_monthly", :via => [:get]
+  match '/city/:id/actual_temp_yearly' => "weather_day#actual_temp_yearly", :via => [:get]
 
 # Rise Set
-  # match '/city/:id/rise_set' => "weather_day#rise_set", :via => [:get]
-  # match '/city/:id/rise_set_monthly' => "weather_day#rise_set_monthly", :via => [:get]
-  # match '/city/:id/rise_set_yearly' => "weather_day#rise_set_yearly", :via => [:get]
+  match '/city/:id/rise_set' => "weather_day#rise_set", :via => [:get]
+  match '/city/:id/rise_set_monthly' => "weather_day#rise_set_monthly", :via => [:get]
+  match '/city/:id/rise_set_yearly' => "weather_day#rise_set_yearly", :via => [:get]
 
 # Rain Snow
-  # match '/city/:id/rain_snow' => "weather_day#rain_snow", :via => [:get]
-  # match '/city/:id/rain_snow_monthly' => "weather_day#rain_snow_monthly", :via => [:get]
-  # match '/city/:id/rain_snow_yearly' => "weather_day#rain_snow_yearly", :via => [:get]
+  match '/city/:id/rain_snow' => "weather_day#rain_snow", :via => [:get]
+  match '/city/:id/rain_snow_monthly' => "weather_day#rain_snow_monthly", :via => [:get]
+  match '/city/:id/rain_snow_yearly' => "weather_day#rain_snow_yearly", :via => [:get]
 
 # Everything Else
-  # match '/city/:id/everything_else' => "weather_day#everything_else", :via => [:get]
-  # match '/city/:id/everything_else_monthly' => "weather_day#everything_else_monthly", :via => [:get]
-  # match '/city/:id/everything_else_yearly' => "weather_day#everything_else_yearly", :via => [:get]
+  match '/city/:id/everything_else' => "weather_day#everything_else", :via => [:get]
+  match '/city/:id/everything_else_monthly' => "weather_day#everything_else_monthly", :via => [:get]
+  match '/city/:id/everything_else_yearly' => "weather_day#everything_else_yearly", :via => [:get]
 
 end
