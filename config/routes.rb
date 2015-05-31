074ada422250cfd79ca850a3c0f8c83f00f5ebcf
Edwin Unger <edwin@ungermail.com>
2015-05-31 12:50:56 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   match '/city/:id/feels_like_monthly' => "weather_day#feels_like_monthly", :via => [:get]
   match '/city/:id/feels_like_yearly' => "weather_day#feels_like_yearly", :via => [:get]
 
-
+  match '/city/:id/actual_temp' => "weather_day#actual_temp", :via => [:get]
+  match '/city/:id/actual_temp_monthly' => "weather_day#actual_temp_monthly", :via => [:get]
+  match '/city/:id/actual_temp_yearly' => "weather_day#actual_temp_yearly", :via => [:get]
 end
