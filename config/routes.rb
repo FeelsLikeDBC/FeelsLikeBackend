5fdb43393dd07d1eb6c138ac5ef6d74ebf29d0bc
Edwin Unger <edwin@ungermail.com>
2015-05-31 10:23:43 -0700

    feels_like routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   match '/ny' => "city#ny", :via => [:get]
   match '/kc' => "city#kc", :via => [:get]
 
-  match '/feels_like' => 'weather_day#feels_like', :via => [:get]
+  match '/sf/feels_like' => 'weather_day#sf_feels_like', :via => [:get]
 
 end
