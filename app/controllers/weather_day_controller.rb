55eddabb00b96191e06cac1fa771744a903eb880
Edwin Unger <edwin@ungermail.com>
2015-06-01 10:24:26 -0700

    rise set routes

--- app/controllers/weather_day_controller.rb
+++ app/controllers/weather_day_controller.rb
@@ -857,12 +857,12 @@ class WeatherDayController < ApplicationController
 end
 
 
-def avg_of_times(array_of_time)
-  size = array_of_time.length
-  avg_minutes = array_of_time.map do |x|
-    hour, minute = x.split(':')
-    total_minutes = hour.to_i * 60 + minute.to_i
-  end
-  .reduce(:+)/size
-  "#{avg_minutes/60}: #{avg_minutes%60}"
-end
+# def avg_of_times(array_of_time)
+#   size = array_of_time.length
+#   avg_minutes = array_of_time.map do |x|
+#     hour, minute = x.split(':')
+#     total_minutes = hour.to_i * 60 + minute.to_i
+#   end
+#   .reduce(:+)/size
+#   "#{avg_minutes/60}: #{avg_minutes%60}"
+# end
